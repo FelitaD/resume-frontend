d080226875359a6b20ad85ad2506acf205a8893c
Donor <donorfelita@msn.com>
2023-12-15 22:12:19 +0100

    Add https and ssl certificate to forwarding rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,10 +77,11 @@ resource "google_compute_url_map" "resume-urlmap" {
   default_service = google_compute_backend_bucket.resume-backend-bucket.id
 }
 
-# HTTP target proxy
-resource "google_compute_target_http_proxy" "resume-lb-http-proxy" {
-  name    = "resume-lb-http-proxy"
+# HTTPS target proxy
+resource "google_compute_target_https_proxy" "resume-lb-https-proxy" {
+  name    = "resume-lb-https-proxy"
   url_map = google_compute_url_map.resume-urlmap.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.resume-ssl-cert.id]
 }
 
 # Forwarding rule
@@ -88,7 +89,7 @@ resource "google_compute_global_forwarding_rule" "resume-forwarding-rule" {
   name                  = "resume-forwarding-rule"
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL_MANAGED"
-  port_range            = "80"
-  target                = google_compute_target_http_proxy.resume-lb-http-proxy.id
+  port_range            = "443"
+  target                = google_compute_target_https_proxy.resume-lb-https-proxy.id
   ip_address            = google_compute_global_address.resume-lb-ip.id
 }
