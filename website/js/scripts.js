f38b061cd35fb6453b8de95209ca08b6095012d2
Donor <donorfelita@msn.com>
2024-02-15 20:44:48 +0100

    Replace images

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -32,3 +32,28 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const apiUrl = 'https://resume-gateway-46hn0g06.ew.gateway.dev/update_visitor_count';
+
+    // Make a CORS request using Fetch API with POST method
+    fetch(apiUrl, { method: 'POST', mode: 'cors' })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Update the visitor count on the HTML page
+            const visitorCountElement = document.getElementById('visitorCount');
+            if (visitorCountElement) {
+                visitorCountElement.textContent = data.count;
+            } else {
+                console.error('Visitor count element not found in the HTML.');
+            }
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
